dfa1e8a63b7476726f302595db564d7758d420ac
LazarBojicic <ahilej1196bc@gmail.com>
2022-08-27 15:56:03 +0200

    new publish

--- docs/_static/nimgame-i18n.sr-Latn.js
+++ docs/_static/nimgame-i18n.sr-Latn.js
@@ -5,6 +5,7 @@ $.i18n().load({
         "nimgame_single_player": "Igraj protiv računara",
         "nimgame_error": "Možes označiti najviše $1 kružić",
         "nimgame_winner" : "Igrač $1 je pobedio.",
-        "nimgame_alg_won" : "Računar je pobedio."
+        "nimgame_alg_won" : "Računar je pobedio.",
+        "nimgame_take_away_msg" : "označi od <span>1</span> do <span>$1</span> kružić"
     }
 })
